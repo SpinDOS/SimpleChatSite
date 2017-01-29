6c1dfec84c2cdda3ff46175048e8076e1931ad61
Alexander <Alexander@ALEXANDER-PC>
2017-01-29 03:08:10 +0300

    Add project files.

---
+++ SimpleChatSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleChatSite.Startup))]
+namespace SimpleChatSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
